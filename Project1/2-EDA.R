12456896f005af20484c4843ea214e8db8c34753
YanweiTong-Iris <yanweito@stanford.edu>
2024-10-03 11:35:52 -0400

    README update

--- Project1/2-EDA.R
+++ Project1/2-EDA.R
@@ -41,7 +41,12 @@ merged_main <- main_data %>%
   dplyr::rename(Race_code = Race..0.Boston..1.Chicago..2.NYC..3.TC..4.D.,
          Sex = Sex..0.F..1.M.,
          Age = Age..yr.) %>%
-  mutate(Gender = factor(ifelse(Sex == 0, "Female", "Male")))
+  mutate(Gender = factor(ifelse(Sex == 0, "Female", "Male"))) %>%
+  mutate(Marathon = case_when(Race_code == 0 ~ "Boston",
+                              Race_code == 1~ "NYC",
+                              Race_code == 2 ~ "Chicago",
+                              Race_code == 3 ~ "Twin Cities",
+                              Race_code == 4 ~ "Grandmas")) 
 
 #Clean up AQI by box
 aqi_box_data = aqi_box_data %>% 
@@ -54,6 +59,7 @@ aqi_box_mean = aqi_box_data %>%
 
 aqi_box_pivot = aqi_box_mean[,c("marathon", "date_local", "parameter_duration", "daily_mean")] %>%
   pivot_wider(names_from = parameter_duration, values_from = daily_mean)
+# ! AQS data by bounding box will not be included in main analysis due to high missingness
 
 #Clean up AQI by CBSA
 aqi_data = aqi_data %>% 
@@ -72,21 +78,114 @@ aqi_pivot = aqi_mean[,c("marathon", "date_local", "parameter_duration", "daily_m
 # Exploratory plotting
 #-----------------------------------------------
 
+# Participant summary
+merged_main %>%
+  mutate(Race = case_when(Race_code == 0 ~ "Boston",
+                          Race_code == 1~ "NYC",
+                          Race_code == 2 ~ "Chicago",
+                          Race_code == 3 ~ "Twin Cities",
+                          Race_code == 4 ~ "Grandmas")) %>%
+  dplyr::select(
+    Race,
+    Gender, 
+    Age
+  ) %>%
+  tbl_summary(
+    statistic = list(
+      all_continuous() ~ "{mean} ({sd})",
+      all_categorical() ~ "{n} ({p}%)"
+    ),
+    by = Race,
+    digits = all_continuous() ~ 2,
+    missing = "no",
+    type = list(
+      Gender ~ "categorical"
+    )
+  ) %>%
+  add_p() %>%
+  modify_caption(caption = "Baseline Characteristics by Race, N = {N}") %>%
+  as_kable_extra(
+    booktabs = TRUE,
+    longtable = TRUE,
+    linesep = ""
+  ) %>%
+  kableExtra::kable_styling(
+    position = "center",
+    latex_options = c("striped", "repeat_header"),
+    stripe_color = "gray!15"
+  )
+
+
+# Environmental summary
+merged_main  %>%
+  dplyr::select(
+    Marathon,
+    WBGT,
+    DP,
+    `Nitrogen dioxide (NO2)-1 HOUR`,
+    `Sulfur dioxide-1 HOUR`,
+    `Ozone-1 HOUR`,
+    `PM2.5 - Local Conditions-1 HOUR`
+  ) %>%
+  distinct() %>%
+  tbl_summary(
+    statistic = all_continuous() ~ c("{mean} ({sd})", "{min}, {max}"),
+    by = Marathon,
+    digits = all_continuous() ~ 2,
+    missing = "no",
+    type = list(
+      WBGT ~ "continuous2",
+      DP ~"continuous2",
+      `Sulfur dioxide-1 HOUR`  ~"continuous2",
+      `Nitrogen dioxide (NO2)-1 HOUR` ~"continuous2",
+      `Ozone-1 HOUR` ~"continuous2",
+      `PM2.5 - Local Conditions-1 HOUR` ~"continuous2"
+    ),
+    label = list(
+      DP = "DP (°C)",
+      `Nitrogen dioxide (NO2)-1 HOUR` = "NO$_2$ (parts per billion)",
+      `Sulfur dioxide-1 HOUR` = "SO$_2$ (parts per billion)",
+      `Ozone-1 HOUR` = "O$_3$, (parts per million)",
+      `PM2.5 - Local Conditions-1 HOUR` = "PM$_{2.5}, (μg/m$^3$)$"
+      # ,
+      # Mat_race = "Maternal race",
+      # SGA = "Small for gestational age",
+      # gender = "Infant gender",
+      # ga = "Obstetrical gestational age (weeks)",
+      # Del_method = "Delivery method",
+      # mat_chorio = "Maternal Chorioamnionitis"
+    )
+  ) %>%
+  modify_caption(caption = "Summary of weather and pollution parameters across five marathons, N = {N}") %>%
+  as_kable_extra(
+    booktabs = TRUE,
+    longtable = TRUE,
+    linesep = ""
+  ) %>%
+  kableExtra::kable_styling(
+    position = "center",
+    latex_options = c("striped", "repeat_header"),
+    stripe_color = "gray!15"
+  )
+
+
+
 # Plot the data with smoothing and 95% CI
 (best_time_gender_age = ggplot(merged_main, aes(x = Age, y = X.CR, color = Gender)) +
-  geom_smooth(method = "loess", se = TRUE) +  # Loess smoothing with 95% CI
+   geom_smooth(method = "loess", se = TRUE, size = 1) +   # Loess smoothing with 95% CI
   labs(
     title = "Men vs Women",
     x = "Age (yrs)",
     y = "Best Time (%CR)"
   ) +
-  scale_y_continuous(limits = c(0, 300)) +  # Adjust y-axis to match the example
+  scale_y_continuous(limits = c(0, 300)) +  
+   scale_color_manual(values = c("Male" = "steelblue", "Female" = "darkred")) + 
   theme_minimal() + 
   theme(
     plot.title = element_text(hjust = 0.5, face = "bold", size = 16),
     axis.title.x = element_text(size = 14),
     axis.title.y = element_text(size = 14),
     legend.title = element_blank(),  # Remove the legend title
-    legend.position = "bottom"  # Move the legend to the bottom
+    legend.position = "bottomright" 
   ))
 
--- Project1/README.md
+++ Project1/README.md
@@ -4,16 +4,24 @@
 
 ### Overview
 
+This exploratory data analysis will focus on the following 3 aims:
+
+1.  Examine effects of increasing age on marathon performance in men and women;
+
+2.  Explore the impact of environmental conditions on marathon performance, and whether the impact differs across age and gender;
+
+3.  Identify the weather parameters (WBGT, Flag conditions, temperature, etc) that have the largest impact on marathon performance.
+
 ### Directory Structure
 
-**`0-references.bib`**: the article bibliography for the main report
+**`0-references.bib`**: the article bibliography for the main report.
 
-**`1-aqi.R`**: extract ambient air quality data from the US EPA Air Quality System Data Mart API for all relevant marathon dates and locations.
+**`1-aqi.R`**: to extract ambient air quality data from the US EPA Air Quality System Data Mart API for all relevant marathon dates and locations.
 
-**`2-EDA.R`**: preliminary and supplementary exploratory data analyses that were not necessarily included in the main report.
+**`2-EDA.R`**: contains preliminary and supplementary exploratory data analyses that were not necessarily included in the main report.
 
-**`3-PHP2550-Project1Main.Rmd`**:
+**`3-PHP2550-Project1Main.Rmd`**: the main EDA report Rmarkdown.
 
-**`3-PHP2550-Project1Main.pdf`**:
+**`3-PHP2550-Project1Main.pdf`**: the knitted report as .pdf.
 
 Contributor: Yanwei (Iris) Tong
